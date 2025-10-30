739d7dc25960c64f3580a7d4b19af9c3968ea462
Hugo Crétois <hugo.cretois1@gmail.com>
2025-10-30 18:49:11 +0100

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -38,6 +38,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 
 # --- Dashboard framework ---
 import streamlit as st
